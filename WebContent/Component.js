80f0510ce2fa7186bc872f4a6d73cc468ffc6476
devendervb12 <info@s-max.in>
2018-02-17 08:37:35 +0530

    component with rootview

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -1,8 +1,7 @@
 
 sap.ui.core.UIComponent.extend("smax.batch28.A1.Component", {
 	
-
-	createContent : function(){
-		return new sap.m.Button({ text : "Save"})
+	metadata : {
+		rootView : "smax.batch28.A1.view.App"
 	}
 })
