fb8cf69b4ad88bc610775548dc783afc752470cc
devendervb12 <info@s-max.in>
2018-02-18 07:59:09 +0530

    Navigation via Router

--- WebContent/controller/ProductDetails.controller.js
+++ WebContent/controller/ProductDetails.controller.js
@@ -1,9 +1,9 @@
-sap.ui.controller("view.ProductList", {
+sap.ui.controller("smax.batch28.A1.controller.ProductDetails", {
 
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf view.ProductList
+* @memberOf view.ProductDetails
 */
 //	onInit: function() {
 //
@@ -12,7 +12,7 @@ sap.ui.controller("view.ProductList", {
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf view.ProductList
+* @memberOf view.ProductDetails
 */
 //	onBeforeRendering: function() {
 //
@@ -21,7 +21,7 @@ sap.ui.controller("view.ProductList", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf view.ProductList
+* @memberOf view.ProductDetails
 */
 //	onAfterRendering: function() {
 //
@@ -29,10 +29,12 @@ sap.ui.controller("view.ProductList", {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf view.ProductList
+* @memberOf view.ProductDetails
 */
 //	onExit: function() {
 //
 //	}
-
+	goBack : function(){
+		this.getOwnerComponent().getRouter().navTo("productListName");
+	}
 });
--- WebContent/controller/ProductList.controller.js
+++ WebContent/controller/ProductList.controller.js
@@ -1,9 +1,9 @@
-sap.ui.controller("view.ProductDetails", {
+sap.ui.controller("smax.batch28.A1.controller.ProductList", {
 
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf view.ProductDetails
+* @memberOf view.ProductList
 */
 //	onInit: function() {
 //
@@ -12,7 +12,7 @@ sap.ui.controller("view.ProductDetails", {
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf view.ProductDetails
+* @memberOf view.ProductList
 */
 //	onBeforeRendering: function() {
 //
@@ -21,7 +21,7 @@ sap.ui.controller("view.ProductDetails", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf view.ProductDetails
+* @memberOf view.ProductList
 */
 //	onAfterRendering: function() {
 //
@@ -29,10 +29,23 @@ sap.ui.controller("view.ProductDetails", {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf view.ProductDetails
+* @memberOf view.ProductList
 */
 //	onExit: function() {
 //
 //	}
-
+	onPress : function(){
+		// navigate to ProductDetails
+		this.getOwnerComponent().getRouter().navTo("productDetailsName");
+	}
 });
+
+
+
+
+
+
+
+
+
+
